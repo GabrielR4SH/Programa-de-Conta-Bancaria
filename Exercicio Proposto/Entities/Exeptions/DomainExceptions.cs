ebbcd78c7dc8209aae5012862d9db09f7931991d
Gabriel <bieelsantana23@hotmail.com>
2020-11-02 21:31:06 -0300

    commit

---
+++ Exercicio Proposto/Entities/Exeptions/DomainExceptions.cs
@@ -0,0 +1,13 @@
+﻿using Exercicio_Proposto.Entities;
+using System;
+
+namespace Exercicio_Proposto.Entities.Exeptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Exercicio Proposto/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using Exercicio_Proposto.Entities;
+using Exercicio_Proposto.Entities.Exeptions;
+
+namespace Exercicio_Proposto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Holder: ");
+            String holder = Console.ReadLine();
+
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account acc = new Account(number, holder, balance, withdrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                acc.Withdraw(amount);
+                Console.WriteLine("New balance: " + acc.Balance.ToString("F2" 
+                    ,CultureInfo.InvariantCulture));
+
+            }
+            catch (DomainException e)
+            {
+
+                Console.WriteLine("WithDraw ERROR!!!!: " + e.Message);
+            }
+        }
+    }
+}
